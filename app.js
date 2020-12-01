8d70473a10aac2feabe9823ee780734636eede59
Thiago Dias <thidias6003@gmail.com>
2020-12-01 09:46:05 -0300

    feat: publicação no heroku

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const { config, engine } = require('express-edge')
+const compression = require('compression')
+
+app.use(compression())
+app.use(express.static('dist'))
+const port = process.env.PORT || 3000
+
+config({ cache: process.env.NODE_ENV === 'production' })
+app.use(engine);
+
+app.set('/view', `${__dirname}/dist`)
+
+app.get('/', function (req, res) {
+    res.render('/index.html');
+})
+app.use((req, res, next) => {
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http"))
+        res.redirect(`https://${req.hostname}${req.url}`);
+    else
+        next();
+});
+
+app.listen(port, () => {
+    console.log('Heroku listening on port %s', port)
+});
